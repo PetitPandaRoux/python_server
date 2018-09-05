336f896b9e57a0f6030fa37b916c743798d4a798
Sakada Ly <lysakada@gmail.com>
2018-09-05 22:57:21 +0200

    added ORM classes

--- database_setup.py
+++ database_setup.py
@@ -10,6 +10,28 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
+class Restaurant(Base):
+
+    __tablename__ = 'restaurant'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    
+
+
+class MenuItem(Base): 
+
+    __tablename__ = 'menu_item'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
 engine = create_engine(
     'sqlite:///restaurantmenu.db'
 )
