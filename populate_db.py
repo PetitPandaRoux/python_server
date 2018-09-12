b1478bf6449fe302c302b8a01f7ea9efc00f3ded
Sakada Ly <lysakada@gmail.com>
2018-09-12 15:41:56 +0200

    configure database done

---
+++ populate_db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metada.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+myFirstRestaurant = Restaurant(name = "Pizza palazio")
+
+session.add(myFirstRestaurant)
+
+session.commit()
