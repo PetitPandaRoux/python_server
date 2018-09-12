87182327b6095e1bcab185aa27602e8530914f19
Sakada Ly <lysakada@gmail.com>
2018-09-12 23:25:22 +0200

    post added

--- webserver.py
+++ webserver.py
@@ -1,4 +1,5 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
 
 class WebServerHandler (BaseHTTPRequestHandler) :
 
@@ -9,7 +10,10 @@ class WebServerHandler (BaseHTTPRequestHandler) :
             self.end_headers()
 
             message = ""
-            message += "<html><body>Hello!</body></html>"
+            message += "<html><body>"
+            message += "<h1>Hello!</h1>"
+            message += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            message += "</body></html>"
             self.wfile.write(message)
             print (message)
             return
@@ -20,13 +24,41 @@ class WebServerHandler (BaseHTTPRequestHandler) :
             self.end_headers()
 
             message = ""
-            message += "<html><body>Hola!<a href ='/hello'>Back to Hello</a></body></html>"
+            message += "<html><body>"
+            message += "<h1>&#161 Hola !</h1>"
+            message += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            message += "</body></html>"
             self.wfile.write(message)
             print (message)
             return
 
         else :
             self.send_error(404, "File pas trouve %s" %self.path)
+    
+    def do_POST(self):
+        try :
+            self.send_response(301)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            ctype, pdict = cgi.parse_header(
+                self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+            
+            output = ""
+            output += "<html><body>"
+            output += "<h2> Okay, how about this : </h2>"
+            output += "<h1> %s </h1>" %messagecontent[0]
+            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            output += "</html></body>"
+            self.wfile.write(output)
+            print (output)
+        except :
+            pass
+
+
 
 def main() :
     try :
