e097dadb533c69382f28eacbca752ccdc9929830
Sakada Ly <lysakada@gmail.com>
2018-09-12 16:38:57 +0200

    db finish

--- populate_db.py
+++ populate_db.py
@@ -3,12 +3,15 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
 engine = create_engine('sqlite:///restaurantmenu.db')
-Base.metada.bind = engine
+Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
 myFirstRestaurant = Restaurant(name = "Pizza palazio")
-
+cheesepizza = MenuItem(name = "Chesse Pizza", description ="made with natural ingredient", course = "Entree", price = "8.99", restaurant = myFirstRestaurant)
 session.add(myFirstRestaurant)
+session.add(cheesepizza)
 
 session.commit()
+
+print(session.query(Restaurant).all())
