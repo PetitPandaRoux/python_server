79ed30103b62b70441c3a1542aa566a4c5b5c5dc
Sakada Ly <lysakada@gmail.com>
2018-09-19 18:46:13 +0200

    added features new menu item

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -30,15 +30,20 @@ def all_restaurants():
     return output
 
 @app.route('/restaurants/<int:restaurant_id>/')
-def restaurant_Menu(restaurant_id):
+def restaurant_menu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     return render_template('menu.html', restaurant = restaurant, items= items)
 
-@app.route('/restaurants/<int:restaurant_id>/new')
+@app.route('/restaurants/<int:restaurant_id>/new', methods = ['GET','POST'])
 def new_menu_item(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    return "new item to add"
+    if request.method == 'POST':
+        new_item = MenuItem(name = request.form['name'], course = request.form['course'], description = request.form['description'], price = request.form['price'],restaurant_id = restaurant_id)
+        session.add(new_item)
+        session.commit()
+        return redirect(url_for('restaurant_menu', restaurant_id = restaurant_id))
+    else :
+        return render_template('new-menu-item.html', restaurant_id = restaurant_id)
 
 @app.route('/restaurants/<int:restaurant_id>/editMenu/<int:menu_id>/')
 def edit_menu_item(restaurant_id, menu_id):
