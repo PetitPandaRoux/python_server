da06f4539149e50543bd5a08e47fc44b4274acf7
Sakada Ly <lysakada@gmail.com>
2018-09-20 13:56:04 +0200

    restart final project

---
+++ final_project.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+
+#Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+
+@app.route('/')
+@app.route('/restaurants/')
+def show_restaurant():
+    return render_template('restaurants.html', restaurants = restaurants, items= items)
+
+@app.route('/restaurant/new/')
+def create_restaurant():
+    return "create a new restaurant"
+
+@app.route('/restaurant/<int:restaurant_id>/edit/')
+def edit_restaurant(restaurant_id):
+    return "edit restaurant"
+
+@app.route('/restaurant/<int:restaurant_id>/delete/')
+def delete_restaurant(restaurant_id):
+    return "delete restaurant"
+
+@app.route('/restaurant/<int:restaurant_id>/')
+@app.route('/restaurant/<int:restaurant_id>/menu/')
+def show_menu(restaurant_id):
+    return render_template('menu.html', restaurant = restaurant_id, items= items)
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/new/')
+def create_menu_item(restaurant_id):
+    return "create new menu item"
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/')
+def edit_menu_item(restaurant_id, menu_id):
+    return "edit menu item"
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/')
+def delete_menu_item(restaurant_id, menu_id):
+    return "delete menu item"
+
+
+if __name__ == '__main__':
+    app.secret_key = "super_secret_key"
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000, threaded = False)
